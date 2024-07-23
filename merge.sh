2841fe1c3f4b54a61dfa51c3a486f795d08369a9
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2024-07-24 00:25:09 +0700

    Handle code server

--- merge.sh
+++ merge.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+if [ ! -f websocket.py ];
+then
+  wget 'https://raw.githubusercontent.com/google/syzygy/master/third_party/websocket-client/websocket.py' -O websocket.py
+fi
+
+if [ ! -f websocket_server.py ];
+then
+  wget 'https://raw.githubusercontent.com/Pithikos/python-websocket-server/master/websocket_server/websocket_server.py' -O websocket_server.py
+fi
+
 sed '882,$d' websocket.py | sed '1,58d' > a
 sed -i '773,781d' a
 sed -i '758,760d' a
@@ -149,3 +161,80 @@ sed -i '79,81d' b
 
 sed -i 's/    /\t/g' b
 sed -i 's/\t/  /g' b
+
+sed -i '74s/):/,\n         on_accept=None, on_close=None, on_message=None, on_periodic=None):/' b
+sed -i '81 i\    self.on_accept = on_accept\n    self.on_close = on_close\n    self.on_message = on_message\n    self.on_periodic = on_periodic\n' b
+
+sed -i '70,184s/_unicast/send_message/g' b
+sed -i '70,184s/_multicast/send_message_to_all/g' b
+sed -i '70,184s/_deny_new_connections/deny_new_connections/g' b
+sed -i '70,184s/_allow_new_connections/allow_new_connections/g' b
+sed -i '70,184s/_shutdown_gracefully/shutdown_gracefully/g' b
+sed -i '70,184s/_shutdown_abruptly/shutdown_abruptly/g' b
+sed -i '70,184s/_disconnect_clients_gracefully/disconnect_clients_gracefully/g' b
+sed -i '70,184s/_disconnect_clients_abruptly/disconnect_clients_abruptly/g' b
+
+sed -i '186 i\  def _calback(self, callback, *args):' b
+sed -i '187 i\    if callback:' b
+sed -i '188 i\      try:' b
+sed -i '189 i\        callback(*args)' b
+sed -i '190 i\      except Exception, e:' b
+sed -i '191 i\        logger.error(e)\n' b
+
+sed -i '133 i\    self._callback(self.on_close, client)' b
+sed -i '132d' b
+
+sed -i '129 i\    self._callback(self.on_accept, client)' b
+sed -i '128d' b
+
+sed -i '106 i\    self._callback(self.on_message, self.handler_to_client(handler), msg)' b
+sed -i '105d' b
+sed -i '70s/TCPServer, API/HTTPServer/' b
+sed -i '23,68d' b
+sed -i '31s/TCPServer/HTTPServer/' b
+sed -i '45s/_run_forever/run_forever/' b
+
+sed -i '47 i\    periodic_thread = None' b
+sed -i '46d' b
+
+sed -i '50 i\\n      if self.on_periodic:' b
+sed -i '52 i\        periodic_thread = threading.Thread(target=self._periodic, args=())' b
+sed -i '53 i\        periodic_thread.setDaemon(True)' b
+sed -i '54 i\        periodic_thread.start()\n' b
+sed -i '49d' b
+
+sed -i '43 i\    self.keep_alive = keep_alive' b
+sed -i '29s/):/,\n         keep_alive=True):/' b
+
+sed -i '62 i\      if periodic_thread:' b
+sed -i '63 i\        self.keep_alive = False' b
+
+sed -i '59 i\      if periodic_thread:' b
+sed -i '60 i\        self.keep_alive = False' b
+
+sed -i '151 i\  def _periodic(self):' b
+sed -i '152 i\    while True:' b
+sed -i '153 i\      time.sleep(0.1)' b
+sed -i '154 i\      if not self.keep_alive:' b
+sed -i '155 i\        return\n' b
+
+sed -i '157 i\      self._callback(self.on_periodic, self)\n' b
+
+sed -i '188 i\  def read_bytes(self, bufsize):' b
+sed -i '189 i\    shortage = bufsize - sum(len(x) for x in self._recv_buffer)' b
+sed -i '190 i\    while shortage > 0:' b
+sed -i '191 i\      bytes = elf.rfile.read(shortage)' b
+sed -i '192 i\      self._recv_buffer.append(bytes)' b
+sed -i '193 i\      shortage -= len(bytes)' b
+sed -i '194 i\    unified = "".join(self._recv_buffer)' b
+sed -i '195 i\    if shortage == 0:' b
+sed -i '196 i\      self._recv_buffer = []' b
+sed -i "197 i\      return bytes(unified, 'utf-8')" b
+sed -i '198 i\    else:' b
+sed -i '199 i\      self._recv_buffer = [unified[bufsize:]]' b
+sed -i "200 i\      return bytes(unified[:bufsize], 'utf-8')" b
+sed -i '186,187d' b
+
+sed -i '178 i\    self._recv_buffer = []' b
+
+sed -i '244,246s/self.rfile.read/self.read_bytes/g' b
